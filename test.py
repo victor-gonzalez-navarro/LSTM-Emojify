baaeaabdb0ce2d5a7671d12201a36df9ae90442b
Gonav <gonav@MacBook-Air-de-Antonio.local>
2018-07-14 13:38:47 -0700

    Upload 1

---
+++ test.py
@@ -0,0 +1,54 @@
+from emo_utils import *
+from keras.models import load_model
+
+X_train, Y_train = read_csv('data/train_emoji.csv')
+maxLen = len(max(X_train, key=len).split())
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4).
+
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this.
+
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+
+    m = X.shape[0]  # number of training examples
+
+    ### START CODE HERE ###
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m, max_len))
+
+    for i in range(m):  # loop over training examples
+
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = (X[i].lower()).split()
+
+        # Initialize j to 0
+        j = 0
+
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            X_indices[i, j] = word_to_index[w]
+            # Increment j to j + 1
+            j = j + 1
+
+    ### END CODE HERE ###
+
+    return X_indices
+
+
+
+model = load_model('model/my_model.h5')
+x_test = np.array(['finally tommorrow i will be back home'])
+X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
+print(x_test[0] +' '+  label_to_emoji(np.argmax(model.predict(X_test_indices))))
